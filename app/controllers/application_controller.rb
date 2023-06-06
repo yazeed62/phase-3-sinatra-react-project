d5741a353e321a53e3247da053d277b0a6b1d779
Yazeed <dee3ayahmed25@gmail.com>
2023-06-06 18:16:29 +0300

    get route to fetch all task

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,10 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
-  
+
   # Add your routes here
-  get "/" do
-    { message: "Good luck with your project!" }.to_json
+  get "/tasks" do
+    tasks = Task.all
+    tasks.to_json
   end
 
 end
