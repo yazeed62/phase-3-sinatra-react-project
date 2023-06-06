2e63053996718945826af4eddbc113d84711e4b7
Yazeed <dee3ayahmed25@gmail.com>
2023-06-06 21:41:31 +0300

    created instance method in task class

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,7 +1,11 @@
 class Task < ActiveRecord::Base
   belongs_to :category
 
-  
+
   validates :title, presence: true
   validates :description, presence: true
+end
+
+def mark_as_completed
+  update(completed: true)
 end
