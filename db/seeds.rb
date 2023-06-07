8d8b606dee508dc737f2b3422efc72ebdb095a6d
Yazeed <dee3ayahmed25@gmail.com>
2023-06-07 10:16:57 +0300

    creating categories in my database

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,4 +3,8 @@ puts "🌱 Seeding spices..."
 Task.destroy_all
 Category.destroy_all
 
+category1 = Category.create(name: 'Work')
+category2 = Category.create(name: 'Personal')
+category3 = Category.create(name: 'Errands')
+
 puts "✅ Done seeding!"
