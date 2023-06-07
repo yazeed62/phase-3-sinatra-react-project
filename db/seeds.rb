5aa9220839cb80974f722d9c18a07c5b22dbe752
Yazeed <dee3ayahmed25@gmail.com>
2023-06-07 10:19:27 +0300

    creating task in my database

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,4 +7,10 @@ category1 = Category.create(name: 'Work')
 category2 = Category.create(name: 'Personal')
 category3 = Category.create(name: 'Errands')
 
+Task.create(title: 'Finish project', description: 'Complete the final report', category: category1)
+Task.create(title: 'Exercise', description: 'Go for a run', category: category2)
+Task.create(title: 'Grocery shopping', description: 'Buy fruits and vegetables', category: category3)
+Task.create(title: 'Pay bills', description: 'Clear pending utility bills', category: category3)
+
+
 puts "✅ Done seeding!"
